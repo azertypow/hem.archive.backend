0bc5a9f49e2de63d86507ae560d8e23c551c9dc8
azertypow <azertypow@gmail.com>
2023-09-22 18:58:44 +0200

    feat: projects api

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -3,7 +3,23 @@
 /** @global Kirby\Cms\Site $site */
 /** @global Kirby\Cms\Page $page */
 
-echo '<pre>';
-//  print_r( $site->find('themes-et-axe-de-recherche')->children() );
-  print_r( $site->find('themes-et-axe-de-recherche')->children()->template('tags-list') );
-echo '</pre>';
+include_once '_phpTools/jsonEncodeKirbyContent.php';
+
+echo json_encode([
+  'projects' => $site->find('projects')->children()->map(function (\Kirby\Cms\Page $project) use ($site) {
+                    return [
+                      'title' => $project->title()->value(),
+//                      todo: author to authors in blueprint
+                      'authors' => array_values($project->author()->toPages()->map(function (\Kirby\Cms\Page $author) {
+                                  return [
+                                      'author'      => $author->title(),
+                                      'firstname' => $author->firstname()->value(),
+                                      'Name'      => $author->name()->value(),
+                                    ];
+                              })->data()),
+                      'cover' => getImageArrayDataInPage($project),
+                      'themes' => $project->themes(),
+                      'axes' => $project->axes(),
+                    ];
+                })
+]);
