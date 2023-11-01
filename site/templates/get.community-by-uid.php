21370157a8e54b7d6d548fef15a1805eba6004bc
azertypow <azertypow@gmail.com>
2023-11-01 16:31:04 +0100

    feat: add research projects api

--- site/templates/get.community-by-uid.php
+++ site/templates/get.community-by-uid.php
@@ -24,6 +24,14 @@ function getCommunityByUID(string $pageUid, Kirby\Cms\App $kirby, Kirby\Cms\Site
       'job'           => $communityPage->job()->value(),
       'jobdetail'     => $communityPage->jobdetail()->value(),
       'mail'          => $communityPage->mail()->value(),
+      'researchProject' => $site->find('projects')->children()->filter(
+        function ($projectItem) use ($communityPage) {
+
+          $articleAuthorArray = array_map( fn($authorItem) => trim($authorItem), explode( ',', $projectItem->content()->author()) );
+
+          return in_array( $communityPage->uri(), $articleAuthorArray );
+        },
+      )->toArray(),
       'publications'  => $communityPage->publications()->toStructure()->map(
         fn($item) => $item->toArray()
       )->data(),
