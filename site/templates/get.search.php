6f10d84ffcee1d1b934b928c4d9f584343ce0b11
azertypow <azertypow@gmail.com>
2023-12-18 12:14:35 +0100

    feat: search optimisation

--- site/templates/get.search.php
+++ site/templates/get.search.php
@@ -3,7 +3,7 @@
 function getSearch(Kirby\Cms\App $kirby, Kirby\Cms\Site $site): array
 {
   $query   = get('q');
-  $minLength = str_word_count($query) > 1 ? 5 : 2;
+  $minLength = str_word_count($query) > 1 ? 3 : 1;
   $result = page('projects')->search($query, [
     'minlength' => $minLength,
   ]);
