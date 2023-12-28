ac1262acef4ff7448655dfcd7552936936dd4f26
azertypow <azertypow@gmail.com>
2023-12-28 17:30:11 +0100

    feat: search optimisation

--- site/templates/get.search.php
+++ site/templates/get.search.php
@@ -3,10 +3,20 @@
 function getSearch(Kirby\Cms\App $kirby, Kirby\Cms\Site $site): array
 {
   $query   = get('q');
-  $minLength = str_word_count($query) > 1 ? 3 : 1;
-  $result = page('projects')->search($query, [
-    'minlength' => $minLength,
-  ]);
+  $minLength = 2;
+  $result = page('projects')->children()->listed()->search($query, [
+    'fields' => ['title', 'text'],
+    'words' => true,
+    'minlength' => 2,
+    'stopwords' => [
+      "je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles",
+      "me", "te", "se", "nous", "vous", "le", "la", "les", "lui", "leur",
+      "moi", "toi", "soi", "eux", "elles", "ceci", "cela", "ceux",
+      "celles", "celles", "personne", "quelqu'un", "rien", "tout", "un", "une",
+      "quelque", "certains", "certaines", "tel", "telle", "tels", "telles",
+      "tout", "toute", "tous", "toutes",
+    ],
+  ])->toArray();
 
   return [
     'query'     => $query,
