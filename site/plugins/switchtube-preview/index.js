c690d6751cf5ba08d38e0779e8932b612956a9ab
azertypow <nico@villa1203.ch>
2026-02-04 15:35:55 +0100

    feat: custom video block plugin

---
+++ site/plugins/switchtube-preview/index.js
@@ -0,0 +1,91 @@
+panel.plugin("hem/switchtube-preview", {
+  blocks: {
+    video: {
+      template: `
+        <k-block-figure
+          class="k-block-type-video-figure"
+          :caption="content.caption"
+          :disabled="disabled"
+          :empty-text="$t('field.blocks.video.placeholder') + ' …'"
+          :is-empty="!videoSrc"
+          empty-icon="video"
+          @open="open"
+          @update="update"
+        >
+          <k-frame ratio="16/9">
+            <iframe
+              v-if="videoSrc.videoPlatform === 'switch'"
+              class="vimeo-player"
+              :src="videoSrc.src"
+              width="1280"
+              height="720"
+              frameborder="0"
+              allow="fullscreen"
+            />
+            <iframe
+              v-else-if="videoSrc.videoPlatform === 'vimeo'"
+              class="vimeo-player"
+              :src="videoSrc.src"
+              width="1280"
+              height="720"
+              frameborder="0"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
+              allowfullscreen
+            />
+            <iframe
+              v-else-if="videoSrc.videoPlatform === 'youtube'"
+              width="1280"
+              height="720"
+              :src="videoSrc.src"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+              />
+          </k-frame>
+        </k-block-figure>
+      `,
+      computed: {
+        videoSrc() {
+          const url = this.content.url ?? "";
+
+
+          const switchMatch = url.match(
+            /tube\.switch\.ch\/videos\/([a-zA-Z0-9]+)/
+          )
+          const vimeoMatch = url.match(
+            /vimeo\.com\/([a-zA-Z0-9]+)/
+          )
+          const youtubeMatch = url.match(
+            /youtu\.be\/([a-zA-Z0-9]+)/
+          )
+
+          if (switchMatch) {
+            return {
+              videoPlatform: "switch",
+              src: "https://tube.switch.ch/embed/" + switchMatch[1],
+            }
+          }
+          else if(vimeoMatch) {
+            return {
+              videoPlatform: "vimeo",
+              src: "https://player.vimeo.com/video/" + vimeoMatch[1],
+            }
+          }
+          else if(youtubeMatch) {
+            return {
+              videoPlatform: "youtube",
+              src: "https://www.youtube.com/embed/" + youtubeMatch[1],
+            }
+          }
+
+          console.error('url invalid: ', url)
+
+          return null
+        },
+      },
+    },
+  },
+});
---
+++ site/plugins/switchtube-preview/index.php
@@ -0,0 +1,3 @@
+<?php
+
+Kirby::plugin('hem/switchtube-preview', []);
